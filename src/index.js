1b06a0a230613147b0f2cb26843bc02e57290b42
AldousWatts <charles.brough2@gmail.com>
2020-12-11 11:01:52 -0600

    add redux, react-redux, and implement in index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 // import reportWebVitals from './reportWebVitals';
+
+const store = createStore();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
