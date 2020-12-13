d03b6e81869ecfff973126f45ddfe0ef88224ba2
AldousWatts <charles.brough2@gmail.com>
2020-12-13 13:13:28 -0600

    add react-toastify & start using notifications

--- src/App.js
+++ src/App.js
@@ -9,25 +9,32 @@ import Header from "./components/Header.js";
 import Footer from "./components/Footer.js";
 import Showcase from "./components/pages/Showcase.js";
 import FormPage from "./components/pages/form-page/FormPage.js";
-import ViewPage from './components/pages/view-page/ViewPage.js';
-import LoginPage from './components/pages/login-page/LoginPage.js';
-import Error404 from './components/pages/Error404.js';
+import ViewPage from "./components/pages/view-page/ViewPage.js";
+import LoginPage from "./components/pages/login-page/LoginPage.js";
+import Error404 from "./components/pages/Error404.js";
+
+//Importing necessary components for notifications through react-toastify
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
 
 function App() {
   return (
     <>
       <Router>
         <Header />
-          <div className="wrapper">
-        <Switch>
+        <div className="wrapper">
+          <Switch>
             <Route exact path="/" component={Showcase} />
             <Route exact path="/form" component={FormPage} />
             <Route exact path="/view" component={ViewPage} />
             <Route exact path="/user" component={LoginPage} />
             <Route path="/" component={Error404} />
-        </Switch>
-          </div>
+          </Switch>
+        </div>
         <Footer />
+        <ToastContainer
+          position="bottom-center"
+        />
       </Router>
     </>
   );
--- src/components/pages/view-page/ViewPage.js
+++ src/components/pages/view-page/ViewPage.js
@@ -1,15 +1,22 @@
 import React, { useState, useEffect } from "react";
 import Axios from "axios";
 import EmployeeTable from "./EmployeeTable";
+import { toast } from "react-toastify";
 
 const ViewPage = () => {
   const [employees, setEmployees] = useState([]);
+
   const getEmployees = async () => {
-    const resp = await Axios.get("http://3.17.206.158:8080/EmployeeServer/employee");
+    const resp = await Axios.get(
+      "http://3.17.206.158:8080/EmployeeServer/employee"
+    );
     setEmployees(resp.data);
   };
 
-  const refreshEmployees = () => getEmployees();
+  const refreshEmployees = () => {
+    getEmployees();
+    toast.dark("Employee list has been refreshed!");
+  };
 
   useEffect(() => {
     getEmployees();
@@ -20,7 +27,7 @@ const ViewPage = () => {
       employees={employees}
       refreshEmployees={refreshEmployees}
     />
-  )
-}
+  );
+};
 
-export default ViewPage
+export default ViewPage;
